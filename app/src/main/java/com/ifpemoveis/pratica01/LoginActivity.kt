f289790175f5f77ee2672a98dcb783256a5c57ee
Richard Albor <albor2010@gmail.com>
2025-06-24 13:55:42 -0300

    Pratica01–Parte2

---
+++ app/src/main/java/com/ifpemoveis/pratica01/LoginActivity.kt
@@ -0,0 +1,112 @@
+package com.ifpemoveis.pratica01
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ifpemoveis.pratica01.ui.theme.Pratica01_Theme
+
+
+
+import android.app.Activity
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pratica01_Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginPage(modifier = Modifier.padding(innerPadding))
+//                    Greeting2(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPage(modifier: Modifier = Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    val activity = LocalContext.current as? Activity
+    Column(
+        modifier = modifier.padding(16.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text(
+            text = "Bem-vindo/a!",
+            fontSize = 24.sp
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            label = { Text(text = "Digite seu e-mail") },
+            modifier = modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { email = it }
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            label = { Text(text = "Digite sua senha") },
+            modifier = modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { password = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = modifier.fillMaxWidth(0.9f)
+        ) {
+            Button(
+                onClick = {
+                    Toast.makeText(activity, "Login OK!", Toast.LENGTH_LONG).show()
+                },
+                enabled = email.isNotEmpty() && password.isNotEmpty(),
+                modifier = Modifier.weight(1f)
+
+            ) {
+                Text("Login")
+            }
+            Button(
+                onClick = { email = ""; password = "" },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("Limpar")
+            }
+        }
+    }
+}
+
+
+
+
