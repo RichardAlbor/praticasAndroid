c054ba0cab36e2f4b8e7b212a0ec63495bb7bee9
Richard Albor <albor2010@gmail.com>
2025-07-09 00:08:40 -0300

    resolucao Erros local.properts do maps.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,10 @@
+val MAPS_API_KEY: String = project.findProperty("MAPS_API_KEY") as String? ?: ""
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
-    id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -18,6 +19,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        resValue("string", "google_maps_key", MAPS_API_KEY)
     }
 
     buildTypes {
@@ -62,9 +64,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    // Google maps
-    implementation("com.google.android.gms:play-services-maps:19.2.0")
-    implementation("com.google.android.gms:play-services-location:21.3.0")
-    // Google maps for compose
-    implementation("com.google.maps.android:maps-compose:2.8.0")
 }
